814620439b3a30aff5721d007cc4252a03284e5a
Deag999 <deag999@gmail.com>
2024-08-10 15:59:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const dateElement = document.getElementById('date');
-    const currentDate = new Date().toDateString();
-    dateElement.textContent = `Сьогодні: ${currentDate}`;
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('feedbackForm');
+    
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        
+        const name = document.getElementById('name').value;
+        const message = document.getElementById('message').value;
+        
+        alert(`Спасибо за ваше сообщение, ${name}! Мы получили следующее сообщение:\n\n"${message}"`);
+        
+        form.reset();
+    });
 });
